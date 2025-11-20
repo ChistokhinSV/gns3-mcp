21876d636a772e36ecb5993f6fd3dd6cee1c9fe8
Sergei Chistokhin <6992480+ChistokhinSV@users.noreply.github.com>
2025-11-20 11:30:31 +0100

    chore: bump version to 0.48.1

--- gns3_mcp/__init__.py
+++ gns3_mcp/__init__.py
@@ -12,7 +12,7 @@ Features:
 - Docker Integration: Configure container networks, read/write files
 """
 
-__version__ = "0.48.0"
+__version__ = "0.48.1"
 __author__ = "Sergei Chistokhin"
 __email__ = "Sergei@Chistokhin.com"
 __license__ = "MIT"
